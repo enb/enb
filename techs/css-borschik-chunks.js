f9e1795b9ed01ee18aa4be8021504d745d65caab
Marat Dulin <mdevils@yandex-team.ru>
2013-03-28 17:03:23 +0400

    css-borschik-chunks fix. Version 0.7.2.

--- techs/css-borschik-chunks.js
+++ techs/css-borschik-chunks.js
@@ -6,25 +6,22 @@ var inherit = require('inherit'),
     crypto = require('crypto'),
     BorschikPreprocessor = require('../lib/preprocess/borschik-preprocessor');
 
-module.exports = inherit(require('./css-chunks'), {
-    configure: function() {
-        this._freeze = this.getOption('freeze', false);
-        this._minify = this.getOption('minify', true);
-    },
-
-    getName: function() {
-        return 'css-borschik-chunks';
-    },
-    _processChunkData: function(sourceFile, data, suffix) {
-        var _this = this,
-            target = this.getTargetName(suffix);
-        return this.node.createTmpFileForTarget(target).then(function(tmpFile) {
-            return (new BorschikPreprocessor()).preprocessFile(sourceFile.fullname, tmpFile, _this._freeze, _this._minify).then(function() {
-                return vowFs.read(tmpFile, 'utf8').then(function(data) {
-                    vowFs.remove(tmpFile);
-                    return data;
+module.exports = require('./css-chunks').buildFlow()
+    .name('css-borschik-chunks')
+    .defineOption('freeze', false)
+    .defineOption('minify', false)
+    .methods({
+        processChunkData: function(sourceFilename, data) {
+            var _this = this,
+                target = this._target;
+            return this.node.createTmpFileForTarget(target).then(function(tmpFile) {
+                return (new BorschikPreprocessor()).preprocessFile(sourceFilename, tmpFile, _this._freeze, _this._minify).then(function() {
+                    return vowFs.read(tmpFile, 'utf8').then(function(data) {
+                        vowFs.remove(tmpFile);
+                        return data;
+                    });
                 });
             });
-        });
-    }
-});
+        }
+    })
+    .createTech();

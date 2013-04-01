1d0f82955d35f69c9c5f3abeb033ef8f24cc5298
Marat Dulin <mdevils@yandex-team.ru>
2013-04-01 19:31:20 +0400

    Version 0.7.6

---
+++ techs/priv-js-i18n.js
@@ -0,0 +1,9 @@
+module.exports = require('../lib/build-flow').create()
+    .name('priv-js-i18n')
+    .target('target', '?.{lang}.priv.js')
+    .defineRequiredOption('lang')
+    .useSourceFilename('allLangTarget', '?.lang.all.js')
+    .useSourceFilename('langTarget', '?.lang.{lang}.js')
+    .useSourceFilename('privJsTarget', '?.priv.js')
+    .justJoinFilesWithComments()
+    .createTech();

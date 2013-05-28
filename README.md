08d5528e0b9fae3be9526d830985460421c8fd1e
andrewblond <andrewblond@yandex.com>
2013-05-28 15:21:46 +0400

    add docs for browser-js tech

--- README.md
+++ README.md
@@ -458,6 +458,22 @@ nodeConfig.addTech([ require('enb/techs/borschik'), {
 } ]);
 ```
 
+browser-js
+----------
+
+Склеивает `vanilla.js`, `js` и `browser.js`-файлы по deps'ам, сохраняет в виде `?.browser.js`.
+
+**Опции**
+
+* *String* **target** — Результирующий таргет. По умолчанию — `?.browser.js`.
+* *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов (его предоставляет технология `files`). По умолчанию — `?.files`.
+
+**Пример**
+
+```javascript
+nodeConfig.addTech(require('enb/techs/browser-js'));
+```
+
 css
 ---
 
--- techs/browser-js.js
+++ techs/browser-js.js
@@ -1,6 +1,19 @@
 /**
  * browser-js
  * ==========
+ *
+ * Склеивает *vanilla.js*, *js* и *browser.js*-файлы по deps'ам, сохраняет в виде `?.browser.js`.
+ *
+ * **Опции**
+ *
+ * * *String* **target** — Результирующий таргет. По умолчанию — `?.browser.js`.
+ * * *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов (его предоставляет технология `files`). По умолчанию — `?.files`.
+ *
+ * **Пример**
+ *
+ * ```javascript
+ * nodeConfig.addTech(require('enb/techs/browser-js'));
+ * ```
  */
 module.exports = require('../lib/build-flow').create()
     .name('browser-js')

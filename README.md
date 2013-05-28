68d606445a4c0f8ada514a9ddf31d1d1ac9a1001
andrewblond <andrewblond@yandex.com>
2013-05-28 15:21:12 +0400

    add docs for vanilla-js tech

--- README.md
+++ README.md
@@ -1225,6 +1225,22 @@ nodeConfig.addTech([ require('enb/techs/symlink'), {
 } ]);
 ```
 
+vanilla-js
+----------
+
+Склеивает `vanilla.js`-файлы по deps'ам, сохраняет в виде `?.vanilla.js`.
+
+**Опции**
+
+* *String* **target** — Результирующий таргет. По умолчанию — `?.vanilla.js`.
+* *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов (его предоставляет технология `files`). По умолчанию — `?.files`.
+
+**Пример**
+
+```javascript
+nodeConfig.addTech(require('enb/techs/vanilla-js'));
+```
+
 xsl
 ---
 
--- techs/vanilla-js.js
+++ techs/vanilla-js.js
@@ -1,6 +1,19 @@
 /**
  * vanilla-js
  * ==========
+ *
+ * Склеивает *vanilla.js*-файлы по deps'ам, сохраняет в виде `?.vanilla.js`.
+ *
+ * **Опции**
+ *
+ * * *String* **target** — Результирующий таргет. По умолчанию — `?.vanilla.js`.
+ * * *String* **filesTarget** — files-таргет, на основе которого получается список исходных файлов (его предоставляет технология `files`). По умолчанию — `?.files`.
+ *
+ * **Пример**
+ *
+ * ```javascript
+ * nodeConfig.addTech(require('enb/techs/vanilla-js'));
+ * ```
  */
 module.exports = require('../lib/build-flow').create()
     .name('vanilla-js')

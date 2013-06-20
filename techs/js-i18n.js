a70c016d25854c5ce4fac4890b515180756025b9
Egor <escaton@yandex-team.ru>
2013-06-20 21:53:31 +0400

    Обратная совместимость для технологии js-i18n

--- techs/js-i18n.js
+++ techs/js-i18n.js
@@ -2,13 +2,13 @@
  * js-i18n
  * =======
  *
- * Берт собранный `js`-файл и добавляет в результат таргет `?.lang.<язык>.js`. Используется с технологией `js` и `i18n-lang-js`
+ * Берт собранный `js`-файл и добавляет в результат таргет `?.lang.<язык>.js`. Используется с технологией `i18n-lang-js`.
  *
  * **Опции**
  *
  * * *String* **target** — Результирующий таргет. По умолчанию — `?.{lang}.js`.
  * * *String* **lang** — Язык, для которого небходимо собрать файл.
- * * *String* **jsTarget** — Файл к которому покдлеится локализация. По умолчанию — `?.js`.
+ * * *String* **jsTarget** — Файл к которому покдлеится локализация. Если не указан, происходит сборка по чанкам `?.js`.
  *
  * **Пример**
  *
@@ -16,12 +16,38 @@
  * nodeConfig.addTech([ require('enb/techs/js-i18n'), { lang: '{lang}' } ]);
  * ```
  */
+
+var fs = require('graceful-fs'),
+    Vow = require('vow'),
+    vowFs = require('vow-fs');
+
 module.exports = require('../lib/build-flow').create()
     .name('js-i18n')
     .target('target', '?.{lang}.js')
     .defineRequiredOption('lang')
-    .useSourceFilename('jsTarget','?.js')
-    .useSourceFilename('allLangTarget', '?.lang.all.js')
-    .useSourceFilename('langTarget', '?.lang.{lang}.js')
-    .justJoinFilesWithComments()
+    .defineOption('jsTarget')
+    .useFileList('js')
+    .useSourceText('allLangTarget', '?.lang.all.js')
+    .useSourceText('langTarget', '?.lang.{lang}.js')
+    .builder(function(jsChunks, allLangTarget, langTarget) {
+
+        var _this = this,
+            sourceTexts = [allLangTarget, langTarget],
+            jsTarget = this.getOption('jsTarget');
+
+        if (jsTarget) {
+            jsTarget = this.node.unmaskTargetName(jsTarget);
+
+            return this.node.requireSources([jsTarget]).then(function() {
+                return vowFs.read(_this.node.resolvePath(jsTarget), 'utf8');
+            }).then(function(jsTargetSourceText) {
+                return [jsTargetSourceText].concat(sourceTexts).join('\n');
+            });
+
+        } else {
+            return this._joinFilesWithComments(jsChunks).then(function(jsResult) {
+                return [jsResult].concat(sourceTexts).join('\n');
+            });
+        }
+    })
     .createTech();

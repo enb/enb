c898816773fd88a0ecf623f082f2481f8d318752
andrewblond <andrewblond@yandex.com>
2014-01-21 17:25:45 +0400

    Added `sublevelDirectories` option to `levels` tech

--- techs/levels.js
+++ techs/levels.js
@@ -5,9 +5,6 @@
  * Собирает информацию об уровнях переопределения проекта, предоставляет `?.levels`. Результат выполнения этой
  * технологии необходим технологиям `enb/techs/deps`, `enb/techs/deps-old` и `enb/techs/files`.
  *
- * Для каждой ноды по умолчанию добавляется уровень `<путь_к_ноде>/blocks`.
- * Например, для ноды `pages/index` — `pages/index/blocks`.
- *
  * **Опции**
  *
  * * *String* **target** — Результирующий таргет. По умолчанию — `?.levels`.
@@ -15,6 +12,9 @@
  *   Вместо строки с путем к уровню может использоваться объект вида
  *   `{path: '/home/user/www/proj/lego/blocks-desktop', check: false}` для того,
  *   чтобы закэшировать содержимое тех уровней переопределения, которые не модифицируются в рамках проекта.
+ * * *(String)[]* **sublevelDirectories** — Список директорий ноды с уровнями переопределения.
+ *   По умолчанию — для каждой ноды добавляется уровень `<путь_к_ноде>/blocks`, например, для ноды
+ *   `pages/index` — `pages/index/blocks`. Каждый следующий указаный уровень может переопределять предыдущий.
  *
  * **Пример**
  *
@@ -29,8 +29,8 @@
  */
 var Level = require('../lib/levels/level');
 var Levels = require('../lib/levels/levels');
-var fs = require('graceful-fs');
 var Vow = require('vow');
+var VowFs = require('../lib/fs/async-fs');
 var inherit = require('inherit');
 
 module.exports = inherit(require('../lib/tech/base-tech'), {
@@ -41,6 +41,7 @@ module.exports = inherit(require('../lib/tech/base-tech'), {
     init: function () {
         this.__base.apply(this, arguments);
         this._levelConfig = this.getRequiredOption('levels');
+        this._sublevelDirectories = this.getOption('sublevelDirectories', ['blocks']);
         this._target = this.node.unmaskTargetName(this.getOption('target', '?.levels'));
     },
 
@@ -50,62 +51,62 @@ module.exports = inherit(require('../lib/tech/base-tech'), {
 
     build: function () {
         var _this = this;
-        var promise = Vow.promise();
-        try {
-            var target = this._target;
-            var levelList = [];
-            var levelsToCache = [];
-            var levelsIndex = {};
-            var cache = this.node.getNodeCache(target);
-            for (var i = 0, l = this._levelConfig.length; i < l; i++) {
-                var levelInfo = this._levelConfig[i];
-                levelInfo = typeof levelInfo === 'object' ? levelInfo : {path: levelInfo};
-                var levelPath = levelInfo.path;
-                var levelKey = 'level:' + levelPath;
-                if (levelsIndex[levelPath]) {
-                    continue;
-                }
-                levelsIndex[levelPath] = true;
-                if (!this.node.buildState[levelKey]) {
-                    var level = new Level(levelPath, this.node.getLevelNamingScheme(levelPath));
-                    if (levelInfo.check === false) {
-                        var blocks = cache.get(levelPath);
-                        if (blocks) {
-                            level.loadFromCache(blocks);
-                        } else {
-                            levelsToCache.push(level);
-                        }
-                    }
-                    this.node.buildState[levelKey] = level;
-                }
-                levelList.push(this.node.buildState[levelKey]);
+        var target = this._target;
+        var levelList = [];
+        var levelsToCache = [];
+        var levelsIndex = {};
+        var cache = this.node.getNodeCache(target);
+
+        for (var i = 0, l = this._levelConfig.length; i < l; i++) {
+            var levelInfo = this._levelConfig[i];
+            levelInfo = typeof levelInfo === 'object' ? levelInfo : {path: levelInfo};
+            var levelPath = levelInfo.path;
+            var levelKey = 'level:' + levelPath;
+            if (levelsIndex[levelPath]) {
+                continue;
             }
-            var pageBlocksPath = this.node.resolvePath('blocks');
-            fs.exists(pageBlocksPath, function (res) {
-                try {
-                    if (res && !levelsIndex[pageBlocksPath]) {
-                        levelsIndex[pageBlocksPath] = true;
-                        levelList.push(new Level(pageBlocksPath));
+            levelsIndex[levelPath] = true;
+            if (!this.node.buildState[levelKey]) {
+                var level = new Level(levelPath, this.node.getLevelNamingScheme(levelPath));
+                if (levelInfo.check === false) {
+                    var blocks = cache.get(levelPath);
+                    if (blocks) {
+                        level.loadFromCache(blocks);
+                    } else {
+                        levelsToCache.push(level);
                     }
-                    return Vow.all(levelList.map(function (level) {
+                }
+                this.node.buildState[levelKey] = level;
+            }
+            levelList.push(this.node.buildState[levelKey]);
+        }
+
+        return VowFs.listDir(_this.node.getPath())
+            .then(function (listDir) {
+                return _this._sublevelDirectories.filter(function (path) {
+                    return listDir.indexOf(path) !== -1;
+                });
+            })
+            .then(function (sublevels) {
+                return Vow.all(sublevels.map(function (path) {
+                    var sublevelPath = _this.node.resolvePath(path);
+                    if (!levelsIndex[sublevelPath]) {
+                        levelsIndex[sublevelPath] = true;
+                        levelList.push(new Level(sublevelPath, _this.node.getLevelNamingScheme(sublevelPath)));
+                    }
+                }));
+            })
+            .then(function () {
+                return Vow.all(levelList.map(function (level) {
                         return level.load();
-                    })).then((function () {
+                    }))
+                    .then(function () {
                         levelsToCache.forEach(function (level) {
                             cache.set(level.getPath(), level.getBlocks());
                         });
                         _this.node.resolveTarget(target, new Levels(levelList));
-                        return promise.fulfill();
-                    }), function (err) {
-                        return promise.reject(err);
                     });
-                } catch (err) {
-                    return promise.reject(err);
-                }
             });
-        } catch (err) {
-            promise.reject(err);
-        }
-        return promise;
     },
 
     clean: function () {}

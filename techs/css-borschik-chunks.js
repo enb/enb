783c716c79a4bd031e2113a7b7f8a731d83b479b
Igor Agarlev <agarlev@gmail.com>
2014-02-17 21:34:51 +0400

    add 'tech' option to css-borschik-chunks tech

--- techs/css-borschik-chunks.js
+++ techs/css-borschik-chunks.js
@@ -16,6 +16,7 @@
  *   (его предоставляет технология `files`). По умолчанию — `?.files`.
  * * *Boolean* **minify** — Минифицировать ли в процессе обработки. По умолчанию — `true`.
  * * *Boolean* **freeze** — Использовать ли фризинг в процессе обработки. По умолчанию — `false`.
+ * * *String* **tech** — Технология сборки. По умолчанию — соответствует расширению исходного таргета.
  *
  * **Пример**
  *
@@ -33,18 +34,20 @@ module.exports = require('./css-chunks').buildFlow()
     .name('css-borschik-chunks')
     .defineOption('freeze', false)
     .defineOption('minify', false)
+    .defineOption('tech', null)
     .methods({
         processChunkData: function (sourceFilename) {
             var _this = this;
             var target = this._target;
             return this.node.createTmpFileForTarget(target).then(function (tmpFile) {
-                return (new BorschikPreprocessor())
-                    .preprocessFile(sourceFilename, tmpFile, _this._freeze, _this._minify).then(function () {
-                        return vowFs.read(tmpFile, 'utf8').then(function (data) {
-                            vowFs.remove(tmpFile);
-                            return data;
-                        });
+                return (new BorschikPreprocessor()).preprocessFile(
+                    sourceFilename, tmpFile, _this._freeze, _this._minify, _this._tech
+                ).then(function () {
+                    return vowFs.read(tmpFile, 'utf8').then(function (data) {
+                        vowFs.remove(tmpFile);
+                        return data;
                     });
+                });
             });
         }
     })

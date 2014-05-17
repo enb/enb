babcf56b96ab2e357a974e2c8437f0429924c1da
Vladimir Rakchaev <rakchaev@ya.ru>
2014-05-17 12:02:24 +0400

    Add support for windows and test

---
+++ test/lib/fs/test.dir-glob.js
@@ -0,0 +1,26 @@
+var dirGlob = require('../../../lib/fs/dir-glob');
+var path = require('path');
+require('chai').should();
+
+describe('lib', function () {
+    describe('fs', function () {
+        describe('dir-glob', function () {
+            var globSync;
+            beforeEach(function () {
+                globSync = dirGlob.globSync;
+            });
+            it('should find directories', function (next) {
+                var rootPath = __dirname;
+                var nodes = 'test-bundles/*';
+                var mask = rootPath + '/' + nodes;
+
+                globSync(mask).should.deep.equal([
+                    rootPath + '/test-bundles/bundle1',
+                    rootPath + '/test-bundles/bundle2'
+                ]);
+
+                next();
+            });
+        });
+    });
+});

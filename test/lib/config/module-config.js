4892ee50da34371fb8540fb2195aa44db6972e7d
Ilia Isupov <matumbaman@yandex-team.ru>
2015-07-28 16:38:07 +0300

    Tests for config/module-config

---
+++ test/lib/config/module-config.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect;
+var ModuleConfig = require('../../../lib/config/module-config');
+
+describe('config/module-config', function () {
+    describe('constructor', function () {
+        it('should call parent constructor', function () {
+            var modeConfig = new ModuleConfig();
+
+            expect(modeConfig._chains).to.be.instanceOf(Array)
+                .and.to.be.empty;
+        });
+    });
+
+    describe('getName', function () {
+        it('should throw error about you need to override getName method', function () {
+            var moduleConfig = new ModuleConfig();
+
+            expect(function () { moduleConfig.getName(); }).to.throw('You should override "getName" method of module.');
+        });
+    });
+});

c77c5208b132ddec14e30c0c6da63c4b0a41316f
Anton Usmansky <cody0@yandex-team.ru>
2016-08-16 17:05:24 +0300

    Use md5 hash as a key

--- test/lib/fs/fs-util.js
+++ test/lib/fs/fs-util.js
@@ -1,17 +1,17 @@
 'use strict';
 
 var fsUtil = require('../../../lib/fs/fs-util');
+var crypto = require('crypto');
 
 describe('fs/fs-util', function () {
     describe('mkHash', function () {
-        it('should return valid filename', function () {
-            var result = fsUtil.mkHash('../../some/file.js');
-            expect(result).to.be.equal('.._.._some_file.js');
-        });
+        it('should return md5 hash', function () {
+            var path = '../../some/file.js';
+            var hash = crypto.createHash('md5').update(path).digest('hex');
 
-        it('should return valid win filename', function () {
-            var result = fsUtil.mkHash('C:\\some dir\\file.js');
-            expect(result).to.be.equal('C__some_dir_file.js');
+            var result = fsUtil.mkHash(path);
+
+            expect(result).to.be.equal(hash);
         });
     });
 });
